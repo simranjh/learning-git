5eddedf5aebfc3fc1f185d73df3801cbbd398bf6
simranjh <92986413+simranjh@users.noreply.github.com>
2023-09-18 15:57:40 +0530

    Create addingrep.java

---
+++ addingrep.java
@@ -0,0 +1,14 @@
+package sammiedoingjava;
+import java.util.*;
+public class addingrep {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num; 
+		System.out.print("Enter number: ");
+		num = scanner.nextInt();
+		
+		
+		int num1= num+11*num+num*111;
+		System.out.println("summation of n + nn+ nnn: " + num1);
+	}
+}
