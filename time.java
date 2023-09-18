f0962ee26c580ed0a0cfb4f1aef7cd8ed7253c8e
simranjh <92986413+simranjh@users.noreply.github.com>
2023-09-18 16:03:08 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,9 @@
+package sammiedoingjava;
+
+import java.time.LocalDateTime;
+
+public class time {
+	public static void main(String[] args) {
+		System.out.println("time: "+ LocalDateTime.now() );
+	}
+}
